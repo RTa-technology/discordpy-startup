2a4ead7237483f9b785848aeb580b6408085858d
RTa-technology <57354947+RTa-technology@users.noreply.github.com>
2020-03-05 20:09:47 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -21,4 +21,16 @@ async def ping(ctx):
 async def neco(ctx):
     await ctx.send("🐈")
 
+class JapaneseHelpCommand(commands.DefaultHelpCommand):
+    def __init__(self):
+        super().__init__()
+        self.commands_heading = "コマンド:"
+        self.no_category = "その他"
+        self.command_attrs["help"] = "コマンド一覧と簡単な説明を表示"
+
+    def get_ending_note(self):
+        return (f"各コマンドの説明: {prefix}help <コマンド名>\n"
+                f"各カテゴリの説明: {prefix}help <カテゴリ名>\n")
+
+    
 bot.run(token)
